88f84a4ac8bc5957d0ff75fabb9d198820d217f2
MacStudio_HOME <ssh521@naver.com>
2024-12-15 13:32:24 +0900

    Menu 추가함

--- resources/js/Pages/LeftMenu.vue
+++ resources/js/Pages/LeftMenu.vue
@@ -1,28 +1,62 @@
 <template>
   <div>
-    <ul>
-      <li class="border-slate-900 bg-slate-900 p-1 hover:text-white mb-2">
-        <Link
-          href="/"
-          class="text-indigo-500 hover:text-white py-2 px-4 w-full rounded-md"
-          >Home</Link
+    <div class="mb-2">
+      <Link
+        class="group flex items-center px-3 py-1 rounded-md transition-all duration-200 hover:bg-[#476329]"
+        href="/"
+      >
+        <icon
+          name="dashboard"
+          class="mr-2 w-5 h-5"
+          :class="isUrl('') ? 'fill-[#ffffff]' : 'fill-[#76b934] group-hover:fill-white'"
+        />
+        <div
+          :class="
+            isUrl('')
+              ? 'text-[#ffffff] font-medium'
+              : 'text-[#76b934] group-hover:text-[#ffffff]'
+          "
         >
-      </li>
-      <li class="border-slate-900 bg-slate-900 p-1 hover:text-white">
-        <Link
-          :href="route('test')"
-          class="text-indigo-500 hover:text-white py-2 px-4 w-full rounded-md"
-          >Test</Link
+          Root
+        </div>
+      </Link>
+    </div>
+    <div class="mb-2">
+      <Link
+        class="group flex items-center px-3 py-1 rounded-md transition-all duration-200 hover:bg-[#476329]"
+        href="/test"
+      >
+        <icon
+          name="printer"
+          class="mr-2 w-5 h-5"
+          :class="
+            isUrl('test') ? 'fill-[#ffffff]' : 'fill-[#76b934] group-hover:fill-white'
+          "
+        />
+        <div
+          :class="
+            isUrl('test')
+              ? 'text-[#ffffff] font-medium'
+              : 'text-[#76b934] group-hover:text-[#ffffff]'
+          "
         >
-      </li>
-    </ul>
+          Test
+        </div>
+      </Link>
+    </div>
   </div>
 </template>
 
-<style scoped>
-ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-}
-</style>
+<script setup>
+import { usePage } from "@inertiajs/vue3";
+import Icon from "@/Components/Icon.vue";
+
+const isUrl = (...urls) => {
+  let currentUrl = usePage().url.slice(1);
+  //console.log(currentUrl)
+  if (urls[0] === "") {
+    return currentUrl === "";
+  }
+  return urls.filter((url) => currentUrl.startsWith(url)).length;
+};
+</script>
